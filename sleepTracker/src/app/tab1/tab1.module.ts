20be592180ee1f9072b91bb9fce82f50d40fd510
maddyfoletta <123123689+maddyfoletta@users.noreply.github.com>
2024-03-07 09:20:15 -0800

    Log Sleep Page

--- sleepTracker/src/app/tab1/tab1.module.ts
+++ sleepTracker/src/app/tab1/tab1.module.ts
@@ -3,18 +3,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { Date1Component } from './date1/date1.component';
+import { Date2Component } from './date2/date2.component';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, Date1Component, Date2Component]
 })
 export class Tab1PageModule {}
--- sleepTracker/src/app/tab1/tab1.page.ts
+++ sleepTracker/src/app/tab1/tab1.page.ts
@@ -5,8 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  selectedDate1: string = "";
+  selectedDate2: string = "";
 
   constructor() {}
 
+  onDate1Selected(selectedDateTime: string) {
+    this.selectedDate1 = selectedDateTime;
+  }
+
+  onDate2Selected(selectedDateTime: string) {
+    this.selectedDate2 = selectedDateTime;
+  }
+
+  getSleepTimes() {
+    console.log("Selected Date 1:", this.selectedDate1);
+    console.log("Selected Date 2:", this.selectedDate2);
+  }
 }
+
+
